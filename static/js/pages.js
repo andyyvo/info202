8286c7b5f2e09cce562f8fc467e723f6eab5e3cd
andyyvo <andyvo@bu.edu>
2021-06-02 18:40:49 -0400

    home and nav edits w js

---
+++ static/js/pages.js
@@ -0,0 +1,7 @@
+document.getElementById("casestudy_bucsa").onclick = function() {
+    location.href = "#";
+};
+
+document.getElementById("casestudy_bumpcall").onclick = function() {
+    location.href = "#";
+};
