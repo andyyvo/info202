e4303ccb7db77d323018c9ac9ee99fdca6a41c32
andyyvo <andyvo@bu.edu>
2021-06-03 01:49:34 -0400

    landing page and footer

--- static/js/pages.js
+++ static/js/pages.js
@@ -5,3 +5,11 @@ document.getElementById("casestudy_bucsa").onclick = function() {
 document.getElementById("casestudy_bumpcall").onclick = function() {
     location.href = "#";
 };
+
+document.getElementById("casestudy_wasteless").onclick = function() {
+    location.href = "#";
+};
+
+document.getElementById("casestudy_productvision").onclick = function() {
+    location.href = "#";
+};
